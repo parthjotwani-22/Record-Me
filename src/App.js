dee312d92915c8c12fa80d41a0e6110b51457f1b
parthjotwani-22 <parth_jotwani@hotmail.com>
2023-08-28 16:15:16 +0530

    App Changes

--- src/App.js
+++ src/App.js
@@ -1,24 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
-
+import "./App.css";
+import LoginScreen from "./Pages/LoginScreen/login";
+import RecordScreen from "./Pages/Record screen/RecordScreen";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route exact path="/" element={<LoginScreen />} />
+        <Route exact path="/record" element={<RecordScreen />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
